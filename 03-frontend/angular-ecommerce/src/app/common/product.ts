f47ba0b9036beeb88e0d7437c5a9e70dc70e991b
bhanukandregula <bhanugates@gmail.com>
2022-05-17 16:24:29 -0400

    Resease 01 - Table with Bootstrap + Backend + Img

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,12 @@
+export class Product {
+    sku: string  = '';
+    name: string = '';
+    description: string = '';
+    unitPrice: number = 0;
+    imageUrl: number = 0;
+    active: boolean = false;
+    unitsInStock: number = 0;
+    dateCreated: Date = new Date();
+    lastUpdated: Date = new Date();
+}
+
